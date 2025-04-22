d3d78ed8e735b026986f191eecdd43dac0c92cca
Aleksei Solovev <soloviov.ae@alidi.ru>
2025-04-22 19:45:40 +0300

    Some changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mindmapr/',
+  base: './',
   server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    },
+    port: 3000
   },
   build: {
     outDir: 'dist',
